d136ab8a842b200e190379a043c7ca310cee1aa3
ColeBardin <cole@bardinfamily.com>
2023-04-05 22:45:02 -0400

    added comments to hw1 code

--- HW1/main.c
+++ HW1/main.c
@@ -1,7 +1,8 @@
 /**
   @mainpage CS 260 - Homework 1
   @section Description
-  Describe how the entire program works. What is it for?
+  Game to guess a number between 0 and 100 that the user chooses.
+  Uses binary searching techniques to guess the user's number.
 
   */
 
@@ -10,21 +11,11 @@
   @author Cole Bardin <cab5721@drexel.edu>
   @date April 05, 2023
   @section DESCRIPTION
-  Write a description of what this file contains here.
+  Runs the number guessing program implementing binary searching
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
-  Does a binary search
-  @param target is what to look for
-  @param array is the sorted array to search
-  @param start is first index
-  @param stop is last index
-  @return number of comparisons
-*/
-int binarySearch(int target, int *array, int start, int stop);
 
 int main(void)
 {
@@ -34,7 +25,7 @@ int main(void)
     int isFound = 0;      /* Variable to indicate whether or not the number is found */
     char responseBuf[16]; /* Char array to hold extra characters read from STDIN */
 
-    /* Print out welcome message */
+    /* Print out welcome message with instructions */
     printf("Think of a number between 0 and 100.\n");
     printf("Responses:\n");
     printf("h or H means Higher\n");
@@ -44,50 +35,38 @@ int main(void)
     /* Loop until the number is found */
     while (isFound == 0)
     {
+		/* Calculate next guess as midpoint between upper and lower bound */
         nextGuess = (upper + lower) / 2;
         printf("Is your number %d?\n", nextGuess);
+
+		/* Get user input */
         scanf("%s", responseBuf);
 
         /* Process user input */
         switch (responseBuf[0])
         {
+		/* Yes response case */
         case 'y':
         case 'Y':
             isFound = 1;
             break;
+		/* Higher response case */
         case 'h':
         case 'H':
             lower = nextGuess + 1;
             break;
+		/* Lower response case */
         case 'l':
         case 'L':
             upper = nextGuess - 1;
             break;
+		/* Unexpected response case */
         default:
             printf("Invalid response!\n");
             break;
         }
     }
+
     printf("Thank you for playing\n");
     return 0;
-}
-
-int binarySearch(int target, int *array, int start, int stop)
-{
-    if (start > stop)
-    {
-        printf("Never Found\n");
-        return 0;
-    }
-    int middle = start + (stop - start) / 2;
-    if (target == array[middle])
-    {
-        printf("I found it!\n");
-        return 1;
-    }
-    if (target < array[middle])
-    {
-        return 1 + binarySearch(target, array, start, middle - 1);
-    }
-    return 1 + binarySearch(target, array, middle + 1, stop);
 }
