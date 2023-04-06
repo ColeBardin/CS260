7b91ee8a58e32f279d0265cb117527a50e287409
ColeBardin <cab572@drexel.edu>
2023-04-06 10:34:05 -0400

    manually fixed style for hw1

--- HW1/main.c
+++ HW1/main.c
@@ -17,13 +17,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void){
     int upper = 100;      /* Upper bound of guesses */
     int lower = 0;        /* Lower bound of guesses */
     int nextGuess;        /* Variable to hold the current guess */
-    int isFound = 0;      /* Variable to indicate whether or not the number is found */
-    char responseBuf[16]; /* Char array to hold extra characters read from STDIN */
+    int isFound = 0;      /* Indicates whether or not the number is found */
+    char responseBuf[16]; /* Holds extra characters read from STDIN */
 
     /* Print out welcome message with instructions */
     printf("Think of a number between 0 and 100.\n");
@@ -33,34 +32,33 @@ int main(void)
     printf("y or Y means yes this number is correct\n");
 
     /* Loop until the number is found */
-    while (isFound == 0)
-    {
-		/* Calculate next guess as midpoint between upper and lower bound */
+    while(isFound == 0){
+        /* Calculate next guess as midpoint between upper and lower
+         * bound */
         nextGuess = (upper + lower) / 2;
         printf("Is your number %d?\n", nextGuess);
 
-		/* Get user input */
+        /* Get user input */
         scanf("%s", responseBuf);
 
         /* Process user input */
-        switch (responseBuf[0])
-        {
-		/* Yes response case */
+        switch(responseBuf[0]){
+            /* Yes response case */
         case 'y':
         case 'Y':
             isFound = 1;
             break;
-		/* Higher response case */
+            /* Higher response case */
         case 'h':
         case 'H':
             lower = nextGuess + 1;
             break;
-		/* Lower response case */
+            /* Lower response case */
         case 'l':
         case 'L':
             upper = nextGuess - 1;
             break;
-		/* Unexpected response case */
+            /* Unexpected response case */
         default:
             printf("Invalid response!\n");
             break;
