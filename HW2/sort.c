d577ae3f5dbfa3d4f21425c1d79216dd5ecaec1f
ColeBardin <cab572@drexel.edu>
2023-04-13 09:25:14 -0400

    added hw2 starter code

---
+++ HW2/sort.c
@@ -0,0 +1,41 @@
+/**
+        @file
+        @author Mark Boady <mwb33@drexel.edu>
+        @date July 20, 2022
+        @section DESCRIPTION
+
+        For your homework assignment, you must implement all the unfinished
+   functions provided in this file.
+
+        You may add new functions, but you may not change the function
+   signatures of anything provided.
+
+ */
+
+#include "sort.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/* Implement this function based on lecture */
+void bubbleSort(int *A, int size) { return; }
+
+/* Implement this function based on lecture */
+void insertSort(int *A, int size) { return; }
+
+/* Implement this function based on lecture */
+void mergeSort(int *A, int size) { return; }
+
+/* Implement this function based on lecture */
+void msort(int *A, int start, int stop) { return; }
+
+/* Implement this function based on lecture */
+void merge(int *A, int start, int middle, int stop) { return; }
+
+/* Implement this function based on lecture */
+void quickSort(int *A, int size) { return; }
+
+/* Implement this function based on lecture */
+void qusort(int *A, int start, int stop) { return; }
+
+/* Implement this function based on lecture */
+int partition(int *A, int start, int stop) { return 0; }
