c0913a2981f83074f9e9e619131b03e7ce2a36f2
ColeBardin <cab572@drexel.edu>
2023-04-20 09:47:24 -0400

    added HW3 starer code and framework for queue.c

---
+++ HW3/queue.c
@@ -0,0 +1,35 @@
+/**
+  @file queue.c
+  @author Cole Bardin <cab5721@drexel.edu>
+  @date April 20, 2023
+  @section DESCRIPTION
+  
+  This is the source file for a classic queue data structure.
+  It includes the definitions for methods Queue.h
+ */
+
+#include "queue.h"
+
+char isEmpty(Queue *Q) {
+
+}
+
+void enqueue(int v, Queue *Q) {
+
+}
+
+int front(Queue *Q) {
+
+}
+
+void dequeue(Queue *Q) {
+
+}
+
+void printQueue(Queue *Q) {
+
+}
+
+void josephus(int n, int m) {
+
+}
