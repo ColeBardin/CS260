804d6931254eb7111bebf8332fff8b1b83a8740a
ColeBardin <cab572@drexel.edu>
2023-04-20 20:35:42 -0400

    finished josephus method

--- HW3/queue.c
+++ HW3/queue.c
@@ -14,13 +14,16 @@
 
 Queue *newQueue() {
     Queue *new = malloc(sizeof(Queue));
+    /* Set HEAD and TAIL to be NULL */
+    new->head = NULL;
+    new->tail = NULL;
     /* Return malloc'd pointer, will return NULL if malloc call fails */
     return new;
 }
 
 char isEmpty(Queue *Q) {
     /* Return 1 when HEAD is NULL, else 0 */
-    return Q->head == NULL;
+    return (Q->head == NULL);
 }
 
 void enqueue(int v, Queue *Q) {
@@ -66,12 +69,12 @@ void dequeue(Queue *Q) {
 		
 		/* Check if element is only Node in Queue */
 		if(toDelete->next == NULL){
-			/* Update head to be second element */
-			Q->head = toDelete->next;
-		}else{
 			/* If element was only NODE in Queue, make HEAD and TAIL NULL */
 			Q->head = NULL;
 			Q->tail = NULL;
+		}else{
+			/* Update head to be second element */
+			Q->head = toDelete->next;
 		}
 
         /* Free first element's memory */
@@ -97,5 +100,69 @@ void printQueue(Queue *Q) {
 }
 
 void josephus(int n, int m) {
-	
+    Queue *Q = newQueue(); /* Create new empty Queue */
+    Node *currentNode; /* Pointer to store current Node */
+    int index; /* Variable used to index Queue */
+    int totalEliminated = 0; /* Counter for eliminated Nodes */
+
+    /* If malloc call failed */
+    if(Q == NULL){
+        printf("Error failed to allocate memory for Josephus method\n");
+        return;
+    }
+
+    /* Add n Nodes with their index as the value */
+    for(index = 0; index < n; index++){
+        enqueue(index, Q);
+    }
+
+    /* Start at HEAD */
+    currentNode = Q->head;
+
+    /* Iterate until n Nodes have been eliminated */
+    while(totalEliminated < n){
+        /* Advance M positions from currentNode */
+        for(index = 0; index < m - 1; ){
+            /* Find next valid Node */
+            if(currentNode->next == NULL){
+                /* If currentNode is TAIL, loop back to HEAD */
+                currentNode = Q->head;
+            }else{
+                /* Else, advance to next Node */
+                currentNode = currentNode->next;
+            }
+
+            /* Skip over already killed Nodes */
+            if(currentNode->value != -1){
+                index++;
+            }
+        }
+        /* Print out current value/index */
+        printf("%d", currentNode->value);
+        /* Mark Node as killed */
+        currentNode->value = -1;
+        /* Count killed NODE */
+        totalEliminated++;
+        /* If this Node was not the last living Node */
+        if(totalEliminated < n){
+            printf(" ");
+            /* Find next living Node */
+            while(currentNode->value == -1){
+                /* Go to next Node, loopback to HEAD if at TAIL */
+                if(currentNode->next == NULL){
+                    currentNode = Q->head;
+                }else{
+                    currentNode = currentNode->next;
+                }
+            }
+        }
+    }
+    printf("\n");
+    
+    /* Dequeue all Nodes, this also frees their memory */
+    for(index = 0; index < n; index++){
+        dequeue(Q);
+    }
+    /* Free memory for Queue */
+    free(Q);
 }
