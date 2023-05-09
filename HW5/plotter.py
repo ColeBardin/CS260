8533dcf514a203caf0c9b24d5084b6ab7c1835fb
ColeBardin <cab572@drexel.edu>
2023-05-09 09:47:48 -0400

    added more starter plot code

--- HW5/plotter.py
+++ HW5/plotter.py
@@ -1,16 +1,20 @@
-import matplotlib as py
+import matplotlib.pyploy as plt
 
 N = []
 runs = [[],[],[],[],[]]
+index = 0
 
 with open("results.csv", "r") as file:
-    N = file.readline().split(',')
-    runs[0] = file.readline().split(',')
-    runs[1] = file.readline().split(',')
-    runs[2] = file.readline().split(',')
-    runs[3] = file.readline().split(',')
-    runs[4] = file.readline().split(',')
+    for line in file.readlines():
+        for entry in line.split(','):
+            if index == 0:
+                N.append(int(entry))
+            else:
+                runs[index - 1].append(float(entry))
+        index+=1
 
-print(N)
-print(runs)
-        
+plt.plot(N, runs[0], 'r')
+plt.plot(N, runs[1], 'o')
+plt.plot(N, runs[2], 'y')
+plt.plot(N, runs[3], 'g')
+plt.plot(N, runs[4], 'b')
