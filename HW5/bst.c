2243ff7868f1cb5abbf48b3760255fd01afc9bae
ColeBardin <cab572@drexel.edu>
2023-05-12 14:23:11 -0400

    hw 5 done

--- HW5/bst.c
+++ HW5/bst.c
@@ -1,9 +1,9 @@
 /**
-  @file bst.c
-  @author Cole Bardin <cab5721@drexel.edu>
-  @date May 02, 2023
-  @section DESCRIPTION
-  Write a description of what this file contains here.
+    @file bst.c
+    @author Cole Bardin <cab5721@drexel.edu>
+    @date May 02, 2023
+    @section DESCRIPTION
+    This is the source file for the Binary Search Tree library
  */
 
 #include "bst.h"
@@ -11,82 +11,97 @@
 #include <stdio.h>
 
 BST *newBST() {
+    /* Allocate new binary search tree and set its root to NULL */
     BST *new = malloc(sizeof(BST));
     new->root = NULL;
     return new;
 }
 
 void deleteBST(BST *T) {
+    /* Repeatedly delete the root value until the Tree is empty */
     while(T->root != NULL){
+        /* This call will also free memory of all the Nodes */
         deleteFromTree(T, T->root->value);
     }
 
+    /* Free memory used for the Tree */
     free(T);
 }
 
 void insert(BST *T, int target) {
+    /* Insert integer and save return value as new root */
     T->root = insertValue(target, T->root);
 }
 
 bool find(BST *T, int target) {
+    /* Call recursive function to search through all nodes for the target */
     return findValue(target, T->root);
 }
 
 void deleteFromTree(BST *T, int target) {
-    if(T->root->left == NULL && T->root->right == NULL && T->root->value == target){
-        free(T->root);
-        T->root = NULL;
-    }else if(find(T, target) == true){
-        /* TODO: verify*/
+    /* If the value exists in the tree */
+    if(find(T, target) == true){
+        /* Call recursive function to delete it, starting from the root */
         T->root = deleteValue(target, T->root);
     }
 }
 
 int min(BST *T) {
-    Node *current = T->root;
-    int min = -1;
-
-    while(current != NULL){
-        min = current->value;
-        current = current->left;
+    /* Error handle NULL BST pointer */
+    if(T == NULL){
+        printf("Error: Cannot find minimum of NULL BST\n");
+        return -1;
     }
 
-    return min;
+    /* Call recursive method to find Min */
+    return findMin(T->root);
 }
 
 void inorder(BST *T) {
+    /* Call the recursive inoder printer method starting at the root */
     inorderWalker(T->root);
+    /* Add newline at end of printing */
     printf("\n");
 }
 
 void preorder(BST *T) {
+    /* Call the recursive preorder printer method starting at the root */
     preorderWalker(T->root);
+    /* Add newline at end of printing */
     printf("\n");
 }
 
 void postorder(BST *T) {
+    /* Call the recursive postorder printer method starting at the root */
     postorderWalker(T->root);
+    /* Add newline at end of printing */
     printf("\n");
 }
 
 int height(BST *T) {
+    /* Call recursive height calculator method */
     return nodeHeight(T->root);
 }
 
 void inorderWalker(Node *current) {
     /* left number right */
     if(current == NULL){
+        /* End recursive branch at NULL node */
         return;
     }
     if(current->left == NULL && current->right == NULL){
+        /* If no children, print out current number */
         printf("%d ", current->value);
     }else if(current->left == NULL){
+        /* If left side is NULL, print number then right side */
         printf("%d ", current->value);
         inorderWalker(current->right);
     }else if(current->right == NULL){
+        /* If right side is NULL, print left side then number */
         inorderWalker(current->left);
         printf("%d ", current->value);
     }else{
+        /* If 2 children, print left, then number, then right */
         inorderWalker(current->left);
         printf("%d ", current->value);
         inorderWalker(current->right);
@@ -96,16 +111,22 @@ void inorderWalker(Node *current) {
 void preorderWalker(Node *current) {
     /* node left right */
     if(current == NULL){
+        /* End recursive branch at NULL node */
         return;
     }
+    /* Otherwise, always print current value first */
     printf("%d ", current->value);
     if(current->left == NULL && current->right == NULL){
+        /* End recursive branch if no children */
         return;
     }else if(current->left == NULL){
+        /* If left side is NULL, print out right */
         preorderWalker(current->right);
     }else if(current->right == NULL){
+        /* If right side is NULL, print out left */
         preorderWalker(current->left);
     }else{
+        /* If 2 children, print left then right */
         preorderWalker(current->left);
         preorderWalker(current->right);
     }
@@ -114,17 +135,22 @@ void preorderWalker(Node *current) {
 void postorderWalker(Node *current) {
     /* left right node */
     if(current == NULL){
+        /* End recursive branch at NULL node */
         return;
     }
     if (current->left == NULL && current->right == NULL) {
+        /* If no children, print current value */
         printf("%d ", current->value);
     } else if (current->left == NULL) {
+        /* If left side is NULL, print right side then current value */
         postorderWalker(current->right);
         printf("%d ", current->value);
     } else if (current->right == NULL) {
+        /* If right side is NULL, print left side then current value */
         postorderWalker(current->left);
         printf("%d ", current->value);
     } else {
+        /* If 2 children, print left side, then right side, then current value */
         postorderWalker(current->left);
         postorderWalker(current->right);
         printf("%d ", current->value);
@@ -133,52 +159,71 @@ void postorderWalker(Node *current) {
 
 Node *insertValue(int target, Node *current) {
     if(current == NULL){
+        /* When arrived at proper insert location, allocate memory and populate data */
         Node* newNode = malloc(sizeof(Node));
         newNode->value = target;
         newNode->left = NULL;
         newNode->right = NULL;
         return newNode;
     }else if(current->value == target){
+        /* If value already exists, skip insertion */
         return current;
     }else if(target < current->value){
+        /* Move to the left if current node is larger than insert target value */
         current->left = insertValue(target, current->left);
         return current;
     }else{
+        /* Move to the right if current node is smaller than insert target value */
         current->right = insertValue(target, current->right);
         return current;
     }
 }
 
 bool findValue(int target, Node *current) {
+    /* Recusively find value from current node */
     if(current == NULL){
+        /* If reached a NULL node, the value does not exist in the Tree, return false */
         return false;
     }else if(current->value == target){
+        /* If value is found, return true */
         return true;
     }else if(target < current->value){
+        /* If target is smaller than the current value, move left */
         return findValue(target, current->left);
     }else{
+        /* If target is larger than the current value, move right */
         return findValue(target, current->right);
     }
 }
 
 int findMin(Node *current) {
+    /* Recursively search move left through the Tree to find the minimum value */
     if(current == NULL){
+        /* If passed NULL node by user, return error */
+        printf("Error: Cannot find minimum with NULL root\n");
         return -1;
     }else if(current->left == NULL){
+        /* If the left value is NULL, this is the minimum */
         return current->value;
     }else{
+        /* If left is not NULL, there is a smaller value in Tree */
         return findMin(current->left);
     }
 }
 
 int nodeHeight(Node *current) {
+    /* Recursive method to find the height of a node */
     if(current->left == NULL && current->right == NULL){
+        /* If no children, height is 0 */
         return 0;
     }else if(current->left == NULL){
+        /* If left side is NULL, continue down right side */
         return nodeHeight(current->right) + 1;
     }else if(current->right == NULL){
+        /* If right side is NULL, continue down left side */
         return nodeHeight(current->left) + 1;
     }else{
+        /* If 2 children, find which side is taller */
         int left = nodeHeight(current->left);
         int right = nodeHeight(current->right);
         if(left > right){
@@ -192,14 +237,19 @@ int nodeHeight(Node *current) {
 }
 
 Node *deleteValue(int target, Node *current) {
-    if (current == NULL) {
+    /* Recursive method to find and delete a value from the tree */
+    if(current == NULL){
+        /* If current node is NULL, value is not found */
         return NULL;
-    } else if (current->value == target) {
+    }else if(current->value == target){
+        /* If value is found, delete the Node */
         return deleteNode(current);
-    } else if (target < current->value) {
+    }else if(target < current->value){
+        /* If the target is smaller than the current one, move left down the tree */
         current->left = deleteValue(target, current->left);
         return current;
-    } else {
+    }else{
+        /* If targer is larger than the current one, move right down the tree */
         current->right = deleteValue(target, current->right);
         return current;
     }
@@ -207,27 +257,24 @@ Node *deleteValue(int target, Node *current) {
 
 Node *deleteNode(Node *current) {
     if(current->left == NULL && current->right == NULL){
-        /* No children */
+        /* If no children, free memory and replace node with NULL */
         free(current);
         return NULL;
     }else if(current->left == NULL){
+        /* If left side is NULL, replace this node with right side */
         Node *temp = current->right;
         free(current);
         return temp;
     }else if(current->right == NULL){
+        /* If right side is NULL, replace this node with left side */
         Node *temp = current->left;
         free(current);
         return temp;
     }else{
-        if(current->right != NULL){
-            int min = findMin(current->right);
-            current->value = min;
-            current->right = deleteValue(min, current->right);
-            return current;
-        }else{
-            Node *temp = current->left;
-            free(current);
-            return temp;
-        }
+        /* If it has 2 children, replace this node with minimum value below it */
+        int min = findMin(current->right);
+        current->value = min;
+        current->right = deleteValue(min, current->right);
+        return current;
     }
 }
--- HW5/heightExp.c
+++ HW5/heightExp.c
@@ -3,7 +3,8 @@
   @author Cole Bardin <cab5721@drexel.edu>
   @date May 04, 2023
   @section DESCRIPTION
-  Write a description of what this file contains here.
+  This file contains a function that will generate 5 random BSTs for heights 2^N for N=1 to N=10
+  It will calculate the average height for all 5 Trees of each size and print out a table with the results
  */
 
 #include <stdio.h>
@@ -12,28 +13,48 @@
 #include <math.h>
 #include "bst.h"
 
-void heightExperiments() {
-    int n, testNum, elements, index;
-    float averageHeight;
+#define NUMTESTS 5
 
+void heightExperiments() {
+    int N; /* Value for log2(Num of BST elements) */
+    int testNum; /* Value to track 5 tests of each N value */
+    int elements; /* Number of elements in BST, 2^N */
+    int index; /* Counter for inserting elements into BST */
+    float averageHeight; /* Keeps track of average height for each N value */
+
+    /* Print out table header in MD table format */
     printf("Experiments (N=Number Element, Table Shows Height)\n");
     printf("| %7s | %7s | %7s | %7s | %7s | %7s | %7s |\n", "N", "T1", "T2", "T3", "T4", "T5", "Average");
     printf("| %7s | %7s | %7s | %7s | %7s | %7s | %7s |\n", "----", "-----", "-----", "-----", "-----", "-----", "-------");
-    for (n = 1; n < 11; n++) {
-      printf("| %7d ", (int)pow(2, n));
+
+    /* Repeat for N from 1 to 10, inclusive */
+    for (N = 1; N < 11; N++) {
+      /* Print out the N value in MD table format */
+      printf("| %7d ", (int)pow(2, N));
+      /* Reset average height for this N value */
       averageHeight = 0.0;
-      for (testNum = 1; testNum < 6; testNum++) {
-        elements = pow(2, n);
+      /* Get the number of elements for this round of tests */
+      elements = pow(2, N);
+      /* Repeat for 5 tests */
+      for (testNum = 1; testNum <= NUMTESTS; testNum++) {
+        /* Create BST and insert 2^N random elements */
         BST *T = newBST();
         for (index = 0; index < elements; index++) {
           insert(T, rand());
         }
-        averageHeight += height(T);
+
+        /* Print out current height in MD table format */
         printf("| %7d ", height(T));
 
+        /* Sum the height of each test for this N value */
+        averageHeight += height(T);
+
+        /* Delete the BST */
         deleteBST(T);
       }
-      averageHeight /= 5;
+      /* Divide by number of tests to get average height */
+      averageHeight /= NUMTESTS;
+      /* Output average height in MD table format with newline */
       printf("| %7.2f |\n", averageHeight);
     }
 
