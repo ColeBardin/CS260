d985ec7740ff4b08ef9db332a4cf508801e048b0
ColeBardin <cab572@drexel.edu>
2023-05-16 09:49:29 -0400

    added heap.c empty file

---
+++ HW6/heap.c
@@ -0,0 +1,65 @@
+/**
+    @file heap.c
+    @author Cole Bardin <cab5721@drexel.edu>
+    @date May 16, 2023
+    @section DESCRIPTION
+    Write a description of what this file contains here.
+ */
+
+#include "heap.h"
+
+void heapSort(int* array, int size) {
+
+}
+
+Heap* makenull(int capacity) {
+
+}
+
+void deleteHeap(Heap* myHeap) {
+
+}
+
+bool empty(Heap* myHeap) {
+
+}
+
+int min(Heap* myHeap) {
+
+}
+
+void deletemin(Heap* myHeap) {
+
+}
+ 
+void downheap(Heap* myHeap, int i) {
+
+}
+
+void insert(int x, Heap* myHeap) {
+
+}
+
+void upheap(struct Heap* myHeap,int i) {
+
+}
+
+int parent(int n) {
+
+}
+
+int leftChild(int n) {
+
+}
+
+int rightChild(int n) {
+
+}
+
+void swap(Heap* myHeap, int i, int j) {
+
+}
+
+void printHeap(Heap* myHeap) {
+
+}
